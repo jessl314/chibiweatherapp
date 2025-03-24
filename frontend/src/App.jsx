b9a5f84f6c0158335eb4120d04c2920bd077604e
jessl314 <jlchrysanthemum456@gmail.com>
2025-03-24 10:25:08 -0700

    app now works and I can search for city and country

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,17 @@
-// import { createSignal } from 'solid-js'
+
 // import solidLogo from './assets/solid.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+
 import LocationSearch from "./components/locationSearch";
 
 
 function App() {
-  // const [count, setCount] = createSignal(0)
 
   return (
     <div className="App">
     <LocationSearch />
-      
     </div>
   )
 }
 
-export default App
+export default App;
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,10 @@
-/* @refresh reload */
-import { render } from 'solid-js/web'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = document.getElementById('root')
 
-render(() => <App />, root)
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+);
