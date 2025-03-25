5c0360b95318044ba40b8064aa48d8f8a06cb60d
jessl314 <jlchrysanthemum456@gmail.com>
2025-03-24 17:52:47 -0700

    updated naming for the component getWeather

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,14 @@
 // import solidLogo from './assets/solid.svg'
 // import viteLogo from '/vite.svg'
 
-import LocationSearch from "./components/locationSearch";
+import LocationWeather from "./components/getWeather";
 
 
 function App() {
 
   return (
     <div className="App">
-    <LocationSearch />
+    <LocationWeather />
     </div>
   )
 }
